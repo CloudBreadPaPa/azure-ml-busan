b566463e95ee34e57b4e6604070e0433cfc64a18
dwkim <daewoo.kim@live.com>
2017-06-28 14:09:09 +0900

    initial commit

---
+++ code/python/ml-Iris.py
@@ -0,0 +1,43 @@
+import urllib2
+# If you are using Python 3+, import urllib instead of urllib2
+
+import json 
+
+
+data =  {
+
+        "Inputs": {
+
+                "input1":
+                {
+                    "ColumnNames": ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"],
+                    "Values": [ [ "1", "1", "1", "1", "" ], ]
+                },        },
+            "GlobalParameters": {
+}
+    }
+
+body = str.encode(json.dumps(data))
+
+url = 'https://ussouthcentral.services.azureml.net/workspaces/cc48e0eb5dcc430b9c34b3e5bf954a89/services/e701113b5dc8436e92a65aa02e8aadc8/execute?api-version=2.0&details=true'
+api_key = '<change here>' # Replace this with the API key for the web service
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib2.Request(url, body, headers) 
+
+try:
+    response = urllib2.urlopen(req)
+
+    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+    # req = urllib.request.Request(url, body, headers) 
+    # response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result) 
+except urllib2.HTTPError, error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+
+    print(json.loads(error.read()))                 
